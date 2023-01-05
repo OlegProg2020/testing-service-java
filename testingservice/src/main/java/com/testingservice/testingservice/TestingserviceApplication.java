36dae26cc3338da11f95b152f0ec41b6efc93108
Oleg Minenko <justprog2020@yandex.ru>
2023-01-05 21:01:07 +0300

    create project

---
+++ testingservice/src/main/java/com/testingservice/testingservice/TestingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.testingservice.testingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingserviceApplication.class, args);
+	}
+
+}
